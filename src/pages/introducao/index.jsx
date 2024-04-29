7d9adcfd6a639ecf77d0e013a45387925cada27b
ojuliocode <juliojuriollif@gmail.com>
2024-04-29 10:27:31 -0300

    chore: subtitulo

--- src/pages/introducao/index.jsx
+++ src/pages/introducao/index.jsx
@@ -4,6 +4,7 @@ export default function Introducao() {
     return (
         <section className="main-content" >
             <h2> O que é programação?</h2>
+            <h3> Um guia prático para novos programadores </h3>
         </section>
     )
 }
