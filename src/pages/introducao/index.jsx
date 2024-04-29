4d9d1f8589a7e898a1215e658e519a82e2c2c16e
juliocloud <juliocesarjuriolli@gmail.com>
2024-04-29 08:52:32 -0300

    feat: pagina introducao

---
+++ src/pages/introducao/index.jsx
@@ -0,0 +1,9 @@
+import "./style.css"
+
+export default function Introducao() {
+    return (
+        <section className="main-content" >
+            <h2> O que é programação?</h2>
+        </section>
+    )
+}
